44f52231d16bf2fb46e22386dcb493fc057a5b13
Ilya <clayevenson@gmail.com>
2023-09-10 23:14:35 +0300

    log_data field added

--- db/create-logger-table.sql
+++ db/create-logger-table.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS logger;
 
 CREATE TABLE IF NOT EXISTS logger
 (
-	log_id UUID PRIMARY KEY,
-	log_date_time TIMESTAMP WITH TIME ZONE
+	log_id UUID     PRIMARY KEY,
+	log_date_time   TIMESTAMP WITH TIME ZONE NOT NULL,
+    log_data        TEXT NOT NULL
 );
