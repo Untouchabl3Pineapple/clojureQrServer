7cfd9f6892387242b6ae1e94053b0ef2d07433cf
sabrs0 <sirazhs0310@gmail.com>
2023-09-11 01:34:03 +0300

    cas added

--- my-web-app/src/my_web_app/core.clj
+++ my-web-app/src/my_web_app/core.clj
@@ -6,6 +6,7 @@
             [com.nopolabs.clozxing.encode :as encode]
             [com.nopolabs.clozxing.decode :as decode]
             [hiccup.core :refer :all]
+            [ring.util.response :as response]
             [ring.util.anti-forgery :refer [anti-forgery-field]]
             [ring.util.response :refer [response]]
             [next.jdbc :as jdbc]
@@ -43,21 +44,7 @@
       [:a.login-button {:href "/login"} "Login"]]]
     [:main content]]))
 
-(defn home-page []
-  (render-page "QR Code Encoder & Decoder"
-               [:div.container
-                [:link {:rel "stylesheet"
-                        :type "text/css"
-                        :href "/css/home.css"}]
-                [:h1 "Welcome to the QR Code Encoder & Decoder"]
-                [:p "Encode and decode QR codes online with ease."]
-                [:div.instructions
-                 [:h2 "How to Use"]
-                 [:p "To encode a message into a QR code, visit the 'Encode' page."]
-                 [:p "To decode a QR code and retrieve the original message, visit the 'Decode' page."]]
-                [:div.get-started
-                 [:a {:href "/qrgenerator" :class "button"} "Encode QR Code"]
-                 [:a {:href "/scanner" :class "button"} "Decode QR Code"]]]))
+
 
 
 (defn scanner-page []
@@ -139,11 +126,104 @@
                     [:h2 (str "Encode message: " text)]]))))
 
 
+
+
+
+
+
+(def global-token (atom "string"))
+(defn- generate-token []
+  (str "valid_token")) ; Генерируем случайный токен (str (rand-int 1000000))
+
+(defn- login-validation [login password]
+  (and (= login "admin") (= password "admin")))
+
+(defn- cas-auth [request]
+  (let [params (get-in request [:params])
+        url (get-in  request [:url])
+        login (get params :login)
+        password (get params :password)]
+       ;; Проверка логина и пароля и генерация токена (пока упрощенно)
+    (if (login-validation login password)
+      (let [token (generate-token)
+         ;   resp (client/get "http:localhost:3000/protected" {:headers {"auth" token}})
+            ]
+        (reset! global-token token)
+        {:status 302
+         :headers {"location" "/home" "auth" token}})
+      (response/content-type
+       (response/response "Incorrect login")
+       "text/html"))))
+
+(defn- verify-token [token]
+  (= token "valid_token")) ; Проверяем, является ли токен действительным
+
+(defn- authenticated? [request]
+  (let [token request];(let [token (get-in request [:headers "Authorization"])]
+    (and token (verify-token token)))) ; Проверяем, авторизован ли пользователь
+
+(defn- unauthorized-response []
+  (response/response {:status 401
+                      :headers {"Content-Type" "application/json"}
+                      :body "{\"error\": \"Unauthorized\"}"})) ; Ответ в случае неавторизованного доступа
+
+
+
+
+(defn home-page [request]
+  (let [;params (get-in request [:query-params]
+        headers (get-in request [:headers])
+        token (get headers :auth);token (get params "auth")
+        ]
+    (if (verify-token (str (deref global-token))) ;token;authenticated? token
+           ;(reset! global-token "string")
+      (render-page "QR Code Encoder & Decoder"
+                   [:div.container
+                    [:link {:rel "stylesheet"
+                            :type "text/css"
+                            :href "/css/home.css"}]
+                    [:h1 "Welcome to the QR Code Encoder & Decoder"]
+                    [:p "Encode and decode QR codes online with ease."]
+                    [:div.instructions
+                     [:h2 "How to Use"]
+                     [:p "To encode a message into a QR code, visit the 'Encode' page."]
+                     [:p "To decode a QR code and retrieve the original message, visit the 'Decode' page."]]
+                    [:div.get-started
+                     [:a {:href "/qrgenerator" :class "button"} "Encode QR Code"]
+                     [:a {:href "/scanner" :class "button"} "Decode QR Code"]]])
+      (response/content-type (response/response (str "Illegal token "  request)) "text/plain")))
+  )
+
+
+
+
+
+
+(defn layout [content]
+  (hiccup.core/html
+   [:html
+    [:head [:title "Authentication Example"]]
+    [:body content]]))
+
+(defn login-form []
+  (hiccup.core/html
+   [:h2 "Login"]
+   [:form {:method "POST" :action "/login"}
+    (anti-forgery-field)
+    [:label {:for "login"} "login: "]
+    [:input {:type "text" :name "login"}]
+    [:br]
+    [:label {:for "password"} "password: "]
+    [:input {:type "password" :name "password"}]
+    [:br]
+    [:input {:type "submit" :value "login"}]]))
+
 (defroutes app-routes
-  (GET "/home" [] (home-page))
+  (GET "/home" [] home-page)
   (GET "/scanner" [] (scanner-page))
   (GET "/logger" [] (logger-page))
-
+  (GET "/login" [] (layout (login-form)))
+  (POST "/login" [] cas-auth)
   (GET "/qrgenerator" [] (qr-page))
   (POST "/qrgenerator" [text] (qr-generator text)))
 
