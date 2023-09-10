cd98de7152820da94d1db8904717dccf15db4c0f
Ilya <clayevenson@gmail.com>
2023-09-11 01:12:47 +0300

    db uuid default gen

--- db/create-logger-table.sql
+++ db/create-logger-table.sql
@@ -1,8 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 DROP TABLE IF EXISTS logger;
 
 CREATE TABLE IF NOT EXISTS logger
 (
-	log_id UUID     PRIMARY KEY,
+	log_id          UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 	log_date_time   TIMESTAMP WITH TIME ZONE NOT NULL,
     log_data        TEXT NOT NULL
 );
